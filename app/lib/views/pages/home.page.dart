1cdb0d8a255d44b8f024d4c1573387eafee2471a
iagobm <ibmuciaccia94@gmail.com>
2021-09-10 14:57:50 -0300

    EndDrawer 1 entrega (falta acertar designe)
    1 entrega do EndDrawer (nao esta completo)

--- app/lib/views/pages/home.page.dart
+++ app/lib/views/pages/home.page.dart
@@ -1,5 +1,6 @@
 import 'package:app/services/noticia.service.dart';
 import 'package:app/themes/style_app.dart';
+import 'package:app/views/widgerts/endDrawer-widget.dart';
 import 'package:app/views/widgerts/lista-manchetes.widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -42,28 +43,30 @@ class _HomePage extends State<HomePage> with SingleTickerProviderStateMixin {
     var asd = service.obterManchetes();
   }
 
-  // route() {
-  //   if (this.usuarioLogado != null) {
-  //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
-  //   } else {
-  //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
-  //   }
-  // }
-
-
   @override
   Widget build(BuildContext context) {
     print(1);
     //tartTime();
     return Scaffold(
+      endDrawer:
+      new EndDrawerWidget(),
         body: new NestedScrollView(
           controller: _scrollViewController,
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) { //<-- headerSliverBuilder
             return <Widget>[
               new SliverAppBar(
+                actions: [
+            Builder(
+                    builder: (context) => IconButton(
+                      icon: Icon(Icons.person),
+                      onPressed: () => Scaffold.of(context).openEndDrawer(),
+                      tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                    ),
+                  ),
+                ],
                 title: Container(
                   height: 20,
-                  child: new Text("CCN"),
+                  child: new Text("Noticias APP"),
                 ),
                 pinned: true,                       //<-- pinned to true
                 floating: true,                     //<-- floating to true
@@ -96,51 +99,5 @@ class _HomePage extends State<HomePage> with SingleTickerProviderStateMixin {
     );
   }
 
-
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   print(1);
-  //   //tartTime();
-  //   return Scaffold(
-  //     body: DefaultTabController(
-  //       length: 2,
-  //       child: Scaffold(
-  //         appBar: AppBar(
-  //           toolbarOpacity: 0.0,
-  //           backgroundColor: ThemeApp.btnLoginEscuro,
-  //           flexibleSpace: TabBar(
-  //               // controller: _tabController,
-  //               indicatorColor: Colors.white,
-  //               tabs: [
-  //                 Tab(
-  //                   child: Text("Manchetes",style: TextStyle(color: Colors.white),),
-  //                 ),Tab(
-  //                   child: Text("Exterior",style: TextStyle(color: Colors.white),),
-  //                 )
-  //               ]),
-  //         ),
-  //         body: TabBarView(
-  //           // controller: _tabController,
-  //           children: [
-  //             Scaffold(
-  //               body: ListaManchetesWidget(),
-  //             ),
-  //             Scaffold(
-  //               // body: ConvocacoesAprovadasWidget(),
-  //             ),
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // void navegarParaHome(BuildContext context) {
-  //   Navigator.push(
-  //     context,
-  //     MaterialPageRoute(builder: (context) => HomePage()),
-  //   );
-  // }
 
 }
---
+++ app/lib/views/widgerts/endDrawer-widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EndDrawerWidget extends StatefulWidget{
+  @override
+  _EndDrawerWidget createState() => _EndDrawerWidget();
+}
+
+class _EndDrawerWidget extends State<EndDrawerWidget>{
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+
+    child: ListView(
+    // Important: Remove any padding from the ListView.
+    padding: EdgeInsets.zero,
+      children: <Widget>[
+        Align(
+          alignment: Alignment.center,
+          child: CircleAvatar(
+
+            radius: 50.0,
+            backgroundColor: const Color(0xFF778899),
+            backgroundImage:
+            NetworkImage("https://www.pinpng.com/pngs/m/131-1315114_png-pain-pain-akatsuki-black-and-white-transparent.png"),
+          ),
+        ),
+
+          const DrawerHeader(
+
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Nagato Pain'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+      ],
+
+  ),
+    );
+  }
+
+}
