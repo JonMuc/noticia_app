323a195a3cb75f265f6f3729e71bce07f0cf0485
iagobm <ibmuciaccia94@gmail.com>
2021-12-20 19:35:26 -0300

    Create comentario-view.model.dart
    criação da classe view comentario

---
+++ app/lib/models/comentario-view.model.dart
@@ -0,0 +1,43 @@
+class ComentarioViewModel{
+  int IdComentario;
+  int IdUsuario;
+  String Mensagem;
+  String Nome;
+  String UrlFoto;
+  DateTime DataComentario;
+  int ComentarioAvaliado;
+  // 1 = CURTIU ,  2 = DESCURTIU , 0 = NAO TEVE ACAO
+  int QuantidadeLike;
+  int QuantidadeDeslike;
+
+
+  ComentarioViewModel.t(
+      this.IdComentario, this.IdUsuario, this.Mensagem, this.Nome);
+
+  ComentarioViewModel.fromJson(Map<String, dynamic> json) {
+    IdComentario = json['IdComentario'];
+    IdUsuario = json['IdUsuario'];
+    Mensagem = json['Mensagem'];
+    Nome = json['Nome'];
+    UrlFoto = json['UrlFoto'];
+    DataComentario = DateTime.parse(json['DataComentario']);
+    QuantidadeLike = json['QuantidadeLike'];
+    QuantidadeDeslike = json['QuantidadeDeslike'];
+    ComentarioAvaliado = json['ComentarioAvaliado'];
+
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['IdComentario'] = this.IdComentario;
+    data['IdUsuario'] = this.IdUsuario;
+    data['Mensagem'] = this.Mensagem;
+    data['Nome'] = this.Nome;
+    data['UrlFoto'] = this.UrlFoto;
+    data['DataComentario'] = this.DataComentario;
+    data['QuantidadeLike'] = this.QuantidadeLike;
+    data['QuantidadeDeslike'] = this.QuantidadeDeslike;
+    data['ComentarioAvaliado'] = this.ComentarioAvaliado;
+    return data;
+  }
+}
